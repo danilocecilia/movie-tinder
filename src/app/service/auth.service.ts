0b3f5e7c5ecda97f232838a0cbc4485960cf9b35
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-04-24 21:00:26 -0300

    AuthService: add auth0 authentication

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import * as auth0 from 'auth0-js';
+import { AUTH_CONFIG } from './auth0-variables';
+@Injectable()
+export class AuthService {
+
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientID,
+    redirectUri: AUTH_CONFIG.callbackURL,
+    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) { }
+
+  public login(username: string, password: string): void {
+    this.auth0.login({
+      realm: 'Username-Password-Authentication',
+      username,
+      password
+    }, (err, authResult) => {
+      if (err) {
+        console.log(err);
+        alert(`Error: ${err.error_description}. Check the console for further details.`);
+        return;
+      } else if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      }
+    });
+  }
+
+  public signup(email: string, password: string): void {
+    this.auth0.redirect.signupAndLogin({
+      connection: 'Username-Password-Authentication',
+      email,
+      password,
+    }, err => {
+      if (err) {
+        console.log(err);
+        alert(`Error: ${err.description}. Check the console for further details.`);
+        return;
+      }
+    });
+  }
+
+  public loginWithGoogle(): void {
+    this.auth0.authorize({
+      connection: 'google-oauth2',
+    });
+  }
+
+  public handleAuthentication(): void {
+    this.auth0.parseHash((err, authResult) => {
+      // tslint:disable-next-line:no-debugger
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if (err) {
+        this.router.navigate(['/home']);
+        console.log(err);
+        alert(`Error: ${err.error}. Check the console for further details.`);
+      }
+    });
+  }
+
+  private setSession(authResult): void {
+    // Set the time that the access token will expire at
+    const expiresAt = JSON.stringify(
+      (authResult.expiresIn * 1000) + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    this.router.navigate(['home']);
+  }
+
+  public logout(): void {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // Go back to the home route
+    this.router.navigate(['/home']);
+  }
+
+  public isAuthenticated(): boolean {
+    // Check whether the current time is past the
+    // access token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
---
+++ src/app/service/auth0-variables.ts
@@ -0,0 +1,18 @@
+interface AuthConfig {
+    clientID: string;
+    domain: string;
+    callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+    clientID: 'ZZPIa2gbVJpsHn_CeNDGbymjaw3ukR05',
+    domain: 'dcecilia.auth0.com',
+    callbackURL: 'http://localhost:3000/callback'
+};
+
+// clientID: 'ZZPIa2gbVJpsHn_CeNDGbymjaw3ukR05',
+//     domain: 'dcecilia.auth0.com',
+//     responseType: 'token id_token',
+//     audience: 'https://dcecilia.auth0.com/userinfo',
+//     redirectUri: 'http://localhost:3000/callback',
+//     scope: 'openid'
