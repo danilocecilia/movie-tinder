dd63891eeb69e5f1a2eae9274802320f08d0a5de
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-04-24 21:01:18 -0300

    general changes and enhances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-//firebase configurarion
-import { AngularFireModule } from "angularfire2";
-import { environment } from "../environments/environment";
-import { AngularFireDatabaseModule } from "angularfire2/database";
+// firebase configurarion
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
-//auth 0
-import { AuthService } from "./service/auth.service";
+// auth 0 service
+import { AuthService } from './service/auth.service';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +31,8 @@ import { LoginComponent } from './login/login.component';
     AngularFireDatabaseModule,
     AngularFirestoreModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
